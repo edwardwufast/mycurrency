5b14678e28c60411648f47736b26db05178c73b3
Edward Wu <edwardwu@cybersoft4u.com>
2019-07-01 13:59:24 +0800

    add analyze and report method in analyzer

--- lib/mycurrency/analyzer.py
+++ lib/mycurrency/analyzer.py
@@ -18,23 +18,49 @@ TOKEN = config['default']['line_token']
 
 class analyzer:
 
+    COLUME_NAME = ('時間', '現金買入', '現金賣出', '即期買入', '即期賣出')
+
     def __init__(self, currency):
         self.currency = currency
         self.data = None
         self.report = None
 
     def fetch_data(self):
-        self.data = twder.past_six_month(self.currency)
+        df = pandas.DataFrame(twder.past_six_month(self.currency), columns=analyzer.COLUME_NAME)
+        df = df.set_index('時間')
+        df.index = pandas.to_datetime(df.index)
+        df = df.iloc[::-1]
+        self.data = df
+
 
     def analyze(self):
-        if self.data:
-            pass
-
+        if type(self.data) == pandas.DataFrame:
+            df = self.data
+            report = {}
+            avg10_df = df.rolling(window=10).mean()
+            avg50_df = df.rolling(window=50).mean()
+            if avg10_df['現金賣出'][-1] > avg50_df['現金賣出'][-1]:
+                report['trend'] = '上漲'
+            else:
+                report['trend'] = '下跌'
+            report['max_price'] = df['現金賣出'].max()
+            report['min_price'] = df['現金賣出'].min()
+            report['min_price_of_month'] = df['現金賣出'][-30:].min()
+            report['max_price_of_month'] = df['現金賣出'][-30:].max()
+            report['today_data'] = df.ix[-1]
+            report['best_banks'] = get_low_currency_price_banks(self.currency)
+            self.report = report
         else:
             print('No data to analyze. Call fetch_data method first.')
 
-    def report(self):
-        pass
+    def send_report(self):
+        try:
+            data = self.report
+            myreport = \
+            f"貨幣：{self.currency}\n趨勢：{data['trend']}\n今日資料：{data['today_data']}\n30日現金賣出高點：{data['max_price_of_month']}\n30日現金賣出低點：{data['min_price_of_month']}"
+            self._send_to_line(myreport)
+        except KeyError:
+            raise('please analyze first')
 
     def _send_to_line(self, msg):
         line_url = "https://notify-api.line.me/api/notify"
--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -5,7 +5,6 @@ def test_fetch_data(jpy_analyzer):
 
     jpy_analyzer.fetch_data()
     print(jpy_analyzer.data)
-    assert jpy_analyzer.data
 
 @pytest.mark.skip()
 def test_send_to_line(jpy_analyzer):
@@ -14,3 +13,10 @@ def test_send_to_line(jpy_analyzer):
 def test_get_low_currency_price_banks():
     get_low_currency_price_banks('JPY')
 
+def test_analyze(jpy_analyzer):
+    jpy_analyzer.analyze()
+    print(jpy_analyzer.report)
+
+def test_report(jpy_analyzer):
+    jpy_analyzer.analyze()
+    jpy_analyzer.send_report()
