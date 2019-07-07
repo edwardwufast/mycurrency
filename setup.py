e330177de330488b5932be3f8f847c22d6047386
Edward Wu <edwardwu@cybersoft4u.com>
2019-07-07 16:33:15 +0800

    modify setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import glob
-
-from distutils.core import setup, Extension
+from setuptools import setup, find_packages
 
 requires = ['pandas', 'xlwings', 'openpyxl', 'html_table_extractor',
             'requests', 'twder']
 
 setup(name='mycurrency',
       version='1.0',
+      packages=find_packages('lib/'),
       package_dir={'': 'lib'},
       scripts=glob.glob('bin/*'),
       install_requires=requires,
-      data_files=[('/etc/mycurrency/', ['mycurrency.config'])]
+      data_files=[('/etc/mycurrency/', ['mycurrency.config'])],
+      include_package_data=True
      )
